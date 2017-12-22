01098449a6da9f6dbac865c8e7991ab052f47675
DLR\neid_ei <eric.neidhardt@dlr.de>
2017-12-22 09:26:49 +0100

    -misc

---
+++ osm-map-wrapper/src/test/kotlin/org/neihdardt/osmwrapper/FoobarTest.kt
@@ -0,0 +1,15 @@
+package org.neihdardt.osmwrapper
+
+import org.junit.Test
+
+/**
+ * Created by neid_ei (eric.neidhardt@dlr.de)
+ * on 22.12.2017.
+ */
+class FoobarTest {
+
+	@Test
+	fun testSuccess() {
+		assert(true)
+	}
+}
